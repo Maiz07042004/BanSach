7db6f84e64dea1304d1d9ff17e870cc5c4251f38
Maiz07042004 <dangmai19112016@gmail.com>
2024-12-11 00:02:08 +0700

    Thêm mua ngay

---
+++ BanSach/DTO/BuyNowRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace BanSach.DTO
+{
+	public class BuyNowRequestDTO
+	{
+		public int UserId { get; set; }                  // ID người dùng
+		public int BookId { get; set; }                 // ID sách được mua
+		public int Quantity { get; set; }               // Số lượng sách
+		public string CustomerName { get; set; }        // Tên khách hàng
+		public string Address { get; set; }             // Địa chỉ giao hàng
+		public string PhoneNumber { get; set; }         // Số điện thoại
+		public string Email { get; set; }               // Email
+		public string OrtherNotes { get; set; }         // Ghi chú nếu có
+	}
+}
